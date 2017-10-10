682406107866ef1e13c708fcf06d28f8a17888bb
ghdata03 <ghdata03@163.com>
2017-10-10 15:27:53 +0800

    use leancloud api

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,13 @@
 from bottle import get,run,Bottle,route
-import os
+import os,leancloud
 
 PORT = int(os.environ['LEANCLOUD_APP_PORT'])
+APP_ID = os.environ['LEANCLOUD_APP_ID']                
+APP_KEY = os.environ['LEANCLOUD_APP_KEY']              
+MASTER_KEY = os.environ['LEANCLOUD_APP_MASTER_KEY']    
+
+leancloud.init(APP_ID, app_key=APP_KEY, master_key=MASTER_KEY)
+leancloud.use_master_key(False)
 
 app = Bottle()
 
